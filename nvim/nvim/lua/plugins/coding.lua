30808b41cc24466df666123c050dcbe6e408767e
David Wamai <96637893+davwamai@users.noreply.github.com>
2024-03-27 11:34:22 -0400

    updating dots

---
+++ nvim/nvim/lua/plugins/coding.lua
@@ -0,0 +1,89 @@
+return {
+	-- Incremental rename
+	{
+		"smjonas/inc-rename.nvim",
+		config = true,
+		cmd = "IncRename",
+	},
+
+	-- Refactoring tool
+	{
+		"ThePrimeagen/refactoring.nvim",
+		keys = {
+			{
+				"<leader>r",
+				function()
+					require("refactoring").select_refactor()
+				end,
+				mode = "v",
+				noremap = true,
+				silent = true,
+				expr = false,
+			},
+		},
+		opts = {},
+	},
+
+	-- Go forward/backward with square brackets
+	{
+		"echasnovski/mini.bracketed",
+		event = "BufReadPost",
+		config = function()
+			local bracketed = require("mini.bracketed")
+			bracketed.setup({
+				file = { suffix = "" },
+				window = { suffix = "" },
+				quickfix = { suffix = "" },
+				yank = { suffix = "" },
+				treesitter = { suffix = "n" },
+			})
+		end,
+	},
+
+	-- Better increase/descrease
+	{
+		"monaqa/dial.nvim",
+    -- stylua: ignore
+    keys = {
+      { "<C-a>", function() return require("dial.map").inc_normal() end, expr = true, desc = "Increment" },
+      { "<C-x>", function() return require("dial.map").dec_normal() end, expr = true, desc = "Decrement" },
+    },
+		config = function()
+			local augend = require("dial.augend")
+			require("dial.config").augends:register_group({
+				default = {
+					augend.integer.alias.decimal,
+					augend.integer.alias.hex,
+					augend.date.alias["%Y/%m/%d"],
+					augend.constant.alias.bool,
+					augend.semver.alias.semver,
+					augend.constant.new({ elements = { "let", "const" } }),
+				},
+			})
+		end,
+	},
+
+	{
+		"nvim-cmp",
+		dependencies = { "hrsh7th/cmp-emoji" },
+		opts = function(_, opts)
+			table.insert(opts.sources, { name = "emoji" })
+		end,
+	},
+	{
+		"lervag/vimtex",
+		init = function()
+			-- Use init for configuration, don't use the more common "config".
+			vim.g["vimtex_view_method"] = "skim"
+			vim.g["vimtex_quickfix_mode"] = 0
+			vim.g["vimtex_view_skim_sync"] = 1
+			vim.g["vimtex_view_skim_activate"] = 1
+			vim.g["vimtex_log_ignore"] = {
+				"Underfull",
+				"Overfull",
+				"specifier changed to",
+				"Token not allowed in a PDF string",
+			}
+		end,
+	},
+}
